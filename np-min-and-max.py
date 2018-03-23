87d1b8a025e50eb72a8e14837a27d04db0c33b14
Maxim Dsouza <maximdsouza87@gmail.com>
2018-03-23 14:00:06 +0530

    np-min-and-max

---
+++ np-min-and-max.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+linenumber=1;
+content_as_string_array="";
+while True:
+    try:
+        line = input()
+        if(linenumber==1):
+            shape=line
+            N, M= [int(s) for s in shape.split(' ')] 
+        elif(linenumber>1):
+            content_as_string_array+=line
+            content_as_string_array+=' '
+        linenumber+=1        
+    except EOFError:
+        break
+array_a =np.array(content_as_string_array[:-1].split(" ")).astype(int)
+array_a=array_a.reshape(N, M)
+print(np.max(np.min(array_a, axis=1)))
