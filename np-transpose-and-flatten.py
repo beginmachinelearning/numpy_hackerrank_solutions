5cf9e574ff30088ccd34d69b83c4d2a98331dce7
Maxim Dsouza <maximdsouza87@gmail.com>
2018-03-23 09:13:11 +0530

    Solution

---
+++ np-transpose-and-flatten.py
@@ -0,0 +1,27 @@
+import numpy as np
+
+contents = ""
+
+linenumber=1;
+content_as_string=""
+while True:
+    try:
+        line = input()
+        if(linenumber==1):
+            shape=line
+        else:
+            content_as_string+=line
+            content_as_string+=' '
+        linenumber+=1
+        contents+=line
+        contents+='\n'
+    except EOFError:
+        break
+N, M= shape.split() 
+
+content_as_string = content_as_string[:-1]
+K=np.array(content_as_string.split(" "))
+K=K.astype(int)
+ip=K.reshape(2,2)
+print(np.transpose(ip))
+print(ip.flatten())
